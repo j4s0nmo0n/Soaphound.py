e1e10752cde1d73ae15436e9be35bf4cf1188ffc
belettetimoree <44747728+j4s0nmo0n@users.noreply.github.com>
2025-08-02 20:19:36 +0200

    Update soaphound.py

--- src/soaphound/soaphound.py
+++ src/soaphound/soaphound.py
@@ -1,9 +1,12 @@
 import logging, sys, os, getpass, json, dns.resolver, datetime, time, zipfile
 from soaphound.lib.cli import gen_cli_args
 from impacket.ldap.ldaptypes import LDAP_SID
-from impacket.examples.utils import parse_target
 from soaphound.ad.adws import ADWSConnect, NTLMAuth
-from soaphound.ad.cache_gen import pull_all_ad_objects, adws_objecttype_guid_map, generate_caches, _generate_individual_caches, adws_object_classes, create_and_combine_soaphound_cache, SOAPHOUND_LDAP_PROPERTIES 
+from soaphound.ad.cache_gen import (
+    pull_all_ad_objects, adws_objecttype_guid_map, generate_caches,
+    _generate_individual_caches, adws_object_classes, create_and_combine_soaphound_cache,
+    SOAPHOUND_LDAP_PROPERTIES
+)
 from soaphound.ad.acls import normalize_name
 
 from soaphound.ad.collectors.domain import collect_domains, format_domains
@@ -20,14 +23,12 @@ from soaphound.ad.collectors.computer import collect_computers, format_computers
 from soaphound.ad.collectors.computer_adws import collect_computers_adws, format_computers_adws
 from soaphound.lib.domain import ADDomain
 
-
 def export_bloodhound_json(data, output_path):
     output_dir = os.path.dirname(output_path)
     if output_dir and not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(clean_bytes(data), f, indent=2)
-        #json.dump(data, f, indent=2)
 
 def safe_export_bloodhound_json(data, output_path):
     if "meta" in data and data["meta"].get("count", 0) > 0:
@@ -58,7 +59,6 @@ def zip_bloodhound_jsons(output_dir, timestamp,  archive_name="BloodHound.zip"):
     print(f"Zip file created at : {archive_path}")
 
 def main():
-
     options = gen_cli_args()
     banner = """
  .oooooo..o                                oooo                                                .o8                             
@@ -86,6 +86,7 @@ oo     .d8P 888   888 d8(  888   888   888  888   888  888   888  888   888   88
         logger.setLevel(logging.DEBUG)
 
     logging.debug(options)
+    print(options)
 
     lm = ''
     nt = ''
@@ -106,39 +107,47 @@ oo     .d8P 888   888 d8(  888   888   888  888   888  888   888  888   888   88
         parser.print_help()
         sys.exit(1)
 
-
     print(banner)
 
-    main_query = "(|(objectCategory=person)(objectClass=msDS-GroupManagedServiceAccount) (objectClass=msDS-ManagedServiceAccount)(objectCategory=computer)(objectCategory=group)(objectClass=organizationalUnit)(objectClass=domain)(objectClass=container)(objectClass=groupPolicyContainer))"
-    main_domain_root_dn = "DC=" + ",DC=".join(options.domain.split('.'))
-    
+    # 1. RootDSE query (Resource)
+    adws_resource = ADWSConnect(options.domain_controller, options.domain, options.username, auth, "Resource")
+    contexts = adws_resource.get_rootdse_contexts(adws_resource._fqdn, adws_resource._nmf)
+    schema_dn = contexts["schemaNamingContext"]
+    default_dn = contexts["defaultNamingContext"]   # Use this DN for all pulls!
+    root_domain_dn = contexts["rootDomainNamingContext"]
+    config_dn = contexts["configurationNamingContext"]
+    naming_contexts = contexts["namingContexts"]
 
-    logging.info(f"Trying to connect to {options.domain_controller} ...")
-    adws_conn = ADWSConnect(options.domain_controller, options.domain, options.username, auth, "Enumeration") 
+    # 2. One Enumeration connection for all pulls
+    adws_enum = ADWSConnect(options.domain_controller, options.domain, options.username, auth, "Enumeration")
 
-    objecttype_guid_map = adws_objecttype_guid_map(adws_conn)
+    # 3. ObjectType GUID Mapping (pass schema_dn)
+    objecttype_guid_map = adws_objecttype_guid_map(adws_enum, schema_dn=schema_dn)
     objecttype_guid_map_normalized = {normalize_name(k): v for k, v in objecttype_guid_map.items()}
-    laps_guid = objecttype_guid_map_normalized.get(normalize_name(("ms-Mcs-AdmPwd")))
-    laps2_guid = objecttype_guid_map_normalized.get(normalize_name(("msLAPS-EncryptedPassword")))
+    laps_guid = objecttype_guid_map_normalized.get(normalize_name("ms-Mcs-AdmPwd"))
+    laps2_guid = objecttype_guid_map_normalized.get(normalize_name("msLAPS-EncryptedPassword"))
     has_laps = laps_guid is not None
     has_laps2 = laps2_guid is not None
-    data_container_main = pull_all_ad_objects(ip=options.domain_controller, domain=options.domain, username=options.username, auth=auth, query=main_query, attributes=SOAPHOUND_LDAP_PROPERTIES)
 
+    main_query = "(|(objectCategory=person)(objectClass=msDS-GroupManagedServiceAccount)(objectClass=msDS-ManagedServiceAccount)(objectCategory=computer)(objectCategory=group)(objectClass=organizationalUnit)(objectClass=domain)(objectClass=container)(objectClass=groupPolicyContainer))"
+
+    # 4. Collecte principale (utilise default_dn pour le base_dn)
+    data_container_main = pull_all_ad_objects(
+        ip=options.domain_controller, domain=options.domain, username=options.username, auth=auth,
+        query=main_query, attributes=SOAPHOUND_LDAP_PROPERTIES, base_dn_override=default_dn
+    )
     all_collected_items = data_container_main.get("objects", [])
 
-    # Generate the SOAPHound cache right now (this is useful for everything else later)
-
-    value_to_id_cache, id_to_type_cache = generate_caches(all_collected_items)
-
-    objs = all_collected_items
-    if not objs:
+    if not all_collected_items:
         logging.error("No objects collected (objs is empty or None)")
         sys.exit(1)
 
+    objs = all_collected_items
+    # Normalisation des objets si besoin
     if (isinstance(objs, list) and len(objs) == 1 and isinstance(objs[0], dict) and all(isinstance(v, list) for v in objs[0].values())):
-            objs = fix_superdict_to_list(objs[0])
+        objs = fix_superdict_to_list(objs[0])
     elif isinstance(objs, dict) and all(isinstance(v, list) for v in objs.values()):
-            objs = fix_superdict_to_list(objs)
+        objs = fix_superdict_to_list(objs)
     else:
         if not objs or not isinstance(objs, list) or not isinstance(objs[0], dict):
             logging.error("Something went wrong with object collected")
@@ -152,23 +161,20 @@ oo     .d8P 888   888 d8(  888   888   888  888   888  888   888  888   888   88
         if isinstance(oc, str):
             obj['objectClass'] = [oc]
         elif oc is None:
-            obj['objectClass'] = [] 
+            obj['objectClass'] = []
 
-
-    create_and_combine_soaphound_cache(objs, main_domain_root_dn, output_dir=options.output_dir)
-    
-    # Generate in-memory caches (for use in the following steps)
-    id_to_type_cache, value_to_id_cache = _generate_individual_caches(objs, main_domain_root_dn)
-
+    # Génère le cache pour SOAPHound
+    create_and_combine_soaphound_cache(objs, default_dn, output_dir=options.output_dir)
+    id_to_type_cache, value_to_id_cache = _generate_individual_caches(objs, default_dn)
 
     logging.info(f"Start collecting ...")
     # Collect and format domains    
     raw_domains = collect_domains(options.domain_controller, options.domain, options.username, auth)
-    domains_bh = format_domains(raw_domains, options.domain, main_domain_root_dn, id_to_type_cache, value_to_id_cache, objs, objecttype_guid_map)
+    domains_bh = format_domains(raw_domains, options.domain, default_dn, id_to_type_cache, value_to_id_cache, objs, objecttype_guid_map)
 
     # Collect and format containers
     raw_containers = collect_containers(options.domain_controller, options.domain, options.username, auth)
-    containers_bh = format_containers(raw_containers, options.domain, main_domain_root_dn, id_to_type_cache, value_to_id_cache, objs, objecttype_guid_map)
+    containers_bh = format_containers(raw_containers, options.domain, default_dn, id_to_type_cache, value_to_id_cache, objs, objecttype_guid_map)
 
     domain_obj = raw_domains[0]  # If only one domain, otherwise adapt according to the context.
     sid_bytes = domain_obj.get("objectSid")
@@ -193,7 +199,7 @@ oo     .d8P 888   888 d8(  888   888   888  888   888  888   888  888   888   88
     groups_bh = format_groups(groups, options.domain, domain_sid, id_to_type_cache, value_to_id_cache, objecttype_guid_map)
 
     # Collect and format Users
-    object_classes = adws_object_classes(adws_conn)
+    object_classes = adws_object_classes(adws_enum)
     users = collect_users(options.domain_controller, options.domain, options.username, auth, adws_object_classes=object_classes)
     users_bh = format_users(users, options.domain, domain_sid, id_to_type_cache, value_to_id_cache, objecttype_guid_map)
 
@@ -203,22 +209,26 @@ oo     .d8P 888   888 d8(  888   888   888  888   888  888   888  888   888   88
 
     # Collect and format Computers
     if (options.collectionmethod == "ADWSOnly"):
-        computers = collect_computers_adws(options.domain_controller, options.domain, options.username, auth, base_dn_override=None, adws_object_classes=object_classes, has_laps=has_laps, has_lapsv2=has_laps2)
+        computers = collect_computers_adws(
+            options.domain_controller, options.domain, options.username, auth,
+            base_dn_override=default_dn, adws_object_classes=object_classes,
+            has_laps=has_laps, has_lapsv2=has_laps2
+        )
         computers_bh = format_computers_adws(computers, options.domain, domain_sid, id_to_type_cache, value_to_id_cache, objecttype_guid_map)
     else:
-        smb_auth = ADAuthentication(username=options.username, password=options.password, domain=options.domain, lm_hash=lm, nt_hash=nt,aeskey='',kdc=options.domain_controller)
+        smb_auth = ADAuthentication(username=options.username, password=options.password, domain=options.domain, lm_hash=lm, nt_hash=nt, aeskey='', kdc=options.domain_controller)
         addomain = ADDomain(
-            name=options.domain,                         # ex: 'JJK.LOCAL' ..and by the way Gojo is stronger than Sukuna
-            netbios_name=None,           # Retrieved from collect_domains, or None if unknown.
-            sid=domain_sid,                      # ex: 'S-1-5-21-...'
-            distinguishedname=main_domain_root_dn  # ex: 'DC=JJK,DC=LOCAL'
+            name=options.domain,
+            netbios_name=None,
+            sid=domain_sid,
+            distinguishedname=default_dn
         )
         addomain.domain = addomain.name
         addomain.auth = smb_auth
         addomain.dnscache = DNSCache()
         addomain.dnsresolver = dns.resolver.Resolver()
         addomain.dns_tcp = dns.resolver.Resolver(); addomain.dns_tcp.use_tcp = True
-        addomain.sidcache = ObjectCache() 
+        addomain.sidcache = ObjectCache()
         addomain.samcache = ObjectCache()
         addomain.computersidcache = ObjectCache()
         addomain.num_domains = 1
@@ -229,13 +239,18 @@ oo     .d8P 888   888 d8(  888   888   888  888   888  888   888  888   888   88
             return None
         addomain.get_domain_by_name = get_domain_by_name
 
-        computers = collect_computers(options.domain_controller, options.domain, options.username, auth, base_dn_override=None, adws_object_classes=object_classes, has_laps=has_laps, has_lapsv2=has_laps2)
-        computers_bh = format_computers(computers, options.domain, domain_sid, adws_conn, id_to_type_cache, value_to_id_cache, objecttype_guid_map, bh_rpc_context=addomain, num_workers=options.worker_num)
-
-
+        computers = collect_computers(
+            options.domain_controller, options.domain, options.username, auth,
+            base_dn_override=default_dn, adws_object_classes=object_classes,
+            has_laps=has_laps, has_lapsv2=has_laps2
+        )
+        computers_bh = format_computers(
+            computers, options.domain, domain_sid, adws_enum,
+            id_to_type_cache, value_to_id_cache, objecttype_guid_map,
+            bh_rpc_context=addomain, num_workers=options.worker_num
+        )
 
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S') + "_"
-
     output_dir = options.output_dir
     safe_export_bloodhound_json(domains_bh, os.path.join(output_dir, timestamp + "domains.json"))
     safe_export_bloodhound_json(containers_bh, os.path.join(output_dir, timestamp + "containers.json"))
