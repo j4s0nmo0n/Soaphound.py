be6619671c3eb58d65adb9511247ec22699e0a58
belettetimoree <44747728+j4s0nmo0n@users.noreply.github.com>
2025-08-06 00:24:49 +0200

    Update domain.py

--- src/soaphound/ad/collectors/domain.py
+++ src/soaphound/ad/collectors/domain.py
@@ -2,31 +2,52 @@ from uuid import UUID
 import logging
 from impacket.ldap.ldaptypes import LDAP_SID
 
+from soaphound.lib.utils import ADUtils
 from soaphound.ad.cache_gen import pull_all_ad_objects, filetime_to_unix, _parse_aces, dedupe_aces,adws_objecttype_guid_map
 from soaphound.ad.adws import WELL_KNOWN_SIDS
 from .container import get_child_objects, BH_TYPE_LABEL_MAP
 from .trust import trust_to_bh_output
 
-def collect_domains(ip, domain, username, auth, base_dn_override=None):
+def collect_domains(ip, domain, username, auth, base_dn_override=None, domain_functionality=None):
     """
-    Collecte les objets de type 'domain' (domainDNS uniquement)
+    Collect domain-type objects (domainDNS only)
     """
     attributes = [
         "name", "objectGUID", "objectSid", "objectClass", "distinguishedName",
-        "nTSecurityDescriptor", "whenCreated", "cn", "gPLink"
+        "nTSecurityDescriptor", "whenCreated", "cn", "gPLink", "msds-behavior-version"
     ]
     domains = pull_all_ad_objects(
         ip=ip,
         domain=domain,
         username=username,
         auth=auth,
-        query="(objectClass=domainDNS)",
+        query="(objectClass=domain)",
         attributes=attributes,
         base_dn_override=base_dn_override
     ).get("objects", [])
     print(f"[INFO] Domains collected : {len(domains)}")
     return domains
 
+
+
+def sid_to_principal_type(sid):
+    sid = sid.upper()
+    # able of well-known SIDs
+    if sid in ADUtils.WELLKNOWN_SIDS:
+        return ADUtils.WELLKNOWN_SIDS[sid][1].capitalize()
+    # Final mapping by RID
+    try:
+        rid = int(sid.split('-')[-1])
+    except Exception:
+        return "Unknown"
+    if rid in [
+        512, 516, 518, 519, 520, 521, 522, 525, 526, 527, 553, 544, 545, 546, 547, 548, 549, 
+        550, 551, 552
+    ]:
+        return "Group"
+    if rid == 500:
+        return "User"
+    return "Unknown"
     
 
 def prefix_well_known_sid(sid: str, domain_name: str, domain_sid: str, well_known_sids=WELL_KNOWN_SIDS):
@@ -34,13 +55,13 @@ def prefix_well_known_sid(sid: str, domain_name: str, domain_sid: str, well_know
     domain_sid = domain_sid.upper()
     if sid.startswith(domain_sid + "-") or sid == domain_sid:
         return sid
-    # Ajoute le préfixe si S-1-5-32-XXX ou dans la table well known
+    # Add the prefix if S-1-5-32-XXX or if it's in the well-known table
     if sid in well_known_sids or sid.startswith("S-1-5-32-"):
         return f"{domain_name.upper()}-{sid}"
     return sid
 
-def format_domains(domains, domain_name, domain_root_dn, id_to_type_cache, value_to_id_cache, all_collected_items, objecttype_guid_map, all_trusts):
-    # Construction du lookup DN parent -> enfants directs (containers, OUs, etc)
+def format_domains(domains, domain_name, domain_root_dn, id_to_type_cache, value_to_id_cache, all_collected_items, objecttype_guid_map, all_trusts, domain_functionality=None):
+    # Construction of the lookup from parent DN to direct children (containers, OUs, etc.)
     childobjects_lookup = {}
     for obj in all_collected_items:
         dn_child = obj.get("distinguishedName")
@@ -72,7 +93,7 @@ def format_domains(domains, domain_name, domain_root_dn, id_to_type_cache, value
             domain_sid_str = sid_bytes.upper()
         domain_guid_str = str(UUID(bytes_le=guid_bytes)) if isinstance(guid_bytes, bytes) else guid_bytes
 
-        # Liens GPO
+        # GPO Link
         raw_gplinks = obj.get("gPLink", [])
         gplink_values = raw_gplinks if isinstance(raw_gplinks, list) else ([raw_gplinks] if raw_gplinks else [])
         main_domain_gplinks = []
@@ -91,7 +112,7 @@ def format_domains(domains, domain_name, domain_root_dn, id_to_type_cache, value
             except Exception as e:
                 logging.warning(f"Could not parse gPLink '{gplink_str}' for domain: {e}")
 
-        # Récupère les ACEs du domaine (tous, pas de filtre)
+        # Retrieve the domain's ACEs (all of them, no filtering)
         aces_domain, is_acl_protected_domain = _parse_aces(
             obj.get("nTSecurityDescriptor"),
             id_to_type_cache,
@@ -99,17 +120,23 @@ def format_domains(domains, domain_name, domain_root_dn, id_to_type_cache, value
             "Domain",object_type_guid_map=objecttype_guid_map
         )
         aces_domain = dedupe_aces(aces_domain)
-        # Préfixe les SIDs well-known comme BloodHound
+        # Prefix well-known SIDs like BloodHound
         for ace in aces_domain:
             ace["PrincipalSID"] = prefix_well_known_sid(ace["PrincipalSID"], domain_name, domain_sid_str)
+        
+        try:
+            functional_level = ADUtils.FUNCTIONAL_LEVELS.get(int(domain_functionality), "Unknown")
+        except (TypeError, ValueError):
+            functional_level = "Unknown"
 
+        
         props = {
             "name": f"{domain_name.upper()}",
             "domain": f"{domain_name.upper()}",
             "domainsid": domain_sid_str,
             "distinguishedname": dn.upper(),
             "description": obj.get("description", ""),
-            "functionallevel": str(obj.get("msDS-Behavior-Version", "2016")),
+            "functionallevel": functional_level,
             "highvalue": True,
             "isaclprotected": is_acl_protected_domain,
             "collected": True,
@@ -130,7 +157,6 @@ def format_domains(domains, domain_name, domain_root_dn, id_to_type_cache, value
             "Trusts": trusts_for_domain,
             "Aces": aces_domain,
             "Links": main_domain_gplinks,
-            #"ChildObjects": childobjects_lookup.get(dn.upper(), []),
             "ChildObjects": child_objects,
             "GPOChanges": {
                 "AffectedComputers": [],
